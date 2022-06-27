7ae5a56fe8ef01457ae7c7cd07f868c462d5c826
Cameron Gudger <cgudge2@lsu.edu>
2022-06-26 23:27:22 -0500

    added cpu and made names better

--- index.js
+++ index.js
@@ -1,27 +1,69 @@
 //Initialize const's
-
+const pvpButton = document.getElementById('pvp');
+const cpuButton = document.getElementById('cpu');
+const input = document.getElementById('input');
+const startGameButtons = document.getElementById('startButtons');
+const playerOne = document.getElementById('playerOne');
+const playerTwo = document.getElementById('playerTwo');
+const nameSpace = document.getElementById('nameSpace');
 const connectFour = document.getElementById('connectFour');
-const playComputer = document.getElementById('cpu');
-const playerVsPlayer = document.getElementById('1v1');
-const resetBtn = document.getElementById('reset');
-
+const resetButton = document.getElementById('resetButton');
+//make reset button invisble
+resetButton.hidden = true;
 //Initialize vars
-var playerTurn = 0;
-var gameState = {
+let playerTurn = 0;
+let gameState = {
 	colHeight: [0, 0, 0, 0, 0, 0, 0],
 	colPieces: [[], [], [], [], [], [], []],
 	rowPieces: [[], [], [], [], [], []],
+	isFirstGame: true,
 };
+let notFullCol = [0, 1, 2, 3, 4, 5, 6];
+let isCPUGame = false;
+let playerOneName;
+let playerTwoName;
 
 // Event listener
 connectFour.addEventListener('click', dropPiece);
-playComputer.addEventListener('click', startGame);
-playerVsPlayer.addEventListener('click', startGame);
-resetBtn.addEventListener('click', reset);
+cpuButton.addEventListener('click', startGameCPU);
+pvpButton.addEventListener('click', startGame);
+resetButton.addEventListener('click', reset);
 
 //Function Declarations
-function startGame() {}
+function startGameCPU() {
+	isCPUGame = true;
+	playerTwoName = 'Computer';
+	startGame();
+}
+function startGame() {
+	resetButton.hidden = false;
+	console.log(playerTwoName);
+	console.log(startGameButtons);
+	input.hidden = true;
+	startGameButtons.hidden = true;
+	if (playerOne.value === '') {
+		playerOneName = 'Player One';
+	} else {
+		playerOneName = playerOne.value;
+	}
+
+	if (!isCPUGame) {
+		if (playerTwo.value === '') {
+			playerTwoName = 'Player Two';
+		} else {
+			playerTwoName = playerTwo.value;
+		}
+	}
+	makeBoard();
+}
 function makeBoard() {
+	if (gameState.isFirstGame) {
+		heading = document.createElement('h2');
+		heading.innerHTML = `<span id = 'h2playerOne'>${playerOneName}</span> vs <span id = 'h2playerTwo'>${playerTwoName}</span>`;
+		nameSpace.appendChild(heading);
+		gameState.isFirstGame = false;
+	}
+
 	for (let k = 0; k < 6; k++) {
 		const row = document.createElement('tr');
 		for (let i = 0; i < 7; i++) {
@@ -32,7 +74,7 @@ function makeBoard() {
 	}
 }
 
-function dropPiece(e) {
+function dropPiece(e, isComputer) {
 	let col = e.target.cellIndex;
 	if (playerTurn % 2 === 0) {
 		connectFour.children[5 - gameState.colHeight[col]].children[
@@ -52,16 +94,22 @@ function dropPiece(e) {
 	playerTurn++;
 	isColumnFull(col);
 	if (hasSomeoneWon()) {
+		for (let i = 0; i < connectFour.children.length; i++) {
+			for (let k = 0; k < 7; k++) {
+				connectFour.children[i].children[k].id = 'full';
+			}
+		}
+
 		let background = '';
 		let innerText = '';
 		if (playerTurn % 2 === 0) {
 			background =
 				'linear-gradient(90deg, rgba(47,39,194,1) 0%, rgba(111,111,215,1) 35%, rgba(0,212,255,1) 100%)';
-			innerText = 'Blue has won! Reset?';
+			innerText = `${playerTwo.value} has won! Reset?`;
 		} else {
 			background =
 				'linear-gradient(90deg, rgba(222,27,81,1) 0%, rgba(238,127,168,1) 34%, rgba(247,182,209,1) 85%)';
-			innerText = 'Red has won! Reset?';
+			innerText = `${playerOne.value} has won! Reset?`;
 		}
 		Toastify({
 			text: innerText,
@@ -69,7 +117,6 @@ function dropPiece(e) {
 			close: true,
 			gravity: 'top', // `top` or `bottom`
 			position: 'center', // `left`, `center` or `right`
-			stopOnFocus: true, // Prevents dismissing of toast on hover
 			style: {
 				background: background,
 			},
@@ -78,13 +125,16 @@ function dropPiece(e) {
 			}, // Callback after click
 		}).showToast();
 	}
-	// console.log(buildDiagonalArrays());
+	if (!isComputer && isCPUGame) {
+		canSomeoneWinNext();
+	}
 }
 function isColumnFull(col) {
 	if (gameState.colHeight[col] > 5) {
 		for (let i = 0; i < connectFour.children.length; i++) {
 			connectFour.children[i].children[col].id = 'full';
 		}
+		notFullCol.splice(col, 1);
 	}
 }
 function hasSomeoneWon() {
@@ -199,9 +249,96 @@ function reset() {
 		colPieces: [[], [], [], [], [], [], []],
 		rowPieces: [[], [], [], [], [], []],
 	};
+	notFullCol = [0, 1, 2, 3, 4, 5, 6];
+}
+
+function computerTurn() {}
+function canSomeoneWinNext() {
+	let computer = {
+		target: {
+			cellIndex:
+				notFullCol[Math.floor(Math.random() * notFullCol.length)],
+		},
+	};
+
+	for (let i = 0; i < gameState.colPieces.length; i++) {
+		if (
+			gameState.colPieces[i].length < 6 &&
+			gameState.colPieces[i][gameState.colPieces[i].length - 1] ===
+				'blue' &&
+			gameState.colPieces[i][gameState.colPieces[i].length - 2] ===
+				'blue' &&
+			gameState.colPieces[i][gameState.colPieces[i].length - 3] === 'blue'
+		) {
+			computer.target.cellIndex = i;
+			dropPiece(computer, true);
+			console.log(`its winnable on col ${i}`);
+			return;
+		}
+	}
+	for (let i = 0; i < gameState.rowPieces.length; i++) {
+		for (let k = 0; k < 4; k++) {
+			let isTherePieceUnder;
+			if (i === 0) {
+				isTherePieceUnder = true;
+			} else {
+				isTherePieceUnder =
+					gameState.rowPieces[i - 1][k + 3] !== undefined;
+			}
+			if (
+				gameState.rowPieces[i][k] === 'blue' &&
+				gameState.rowPieces[i][k + 1] === 'blue' &&
+				gameState.rowPieces[i][k + 2] === 'blue' &&
+				gameState.rowPieces[i][k + 3] === undefined &&
+				isTherePieceUnder
+			) {
+				computer.target.cellIndex = k + 3;
+				dropPiece(computer, true);
+				console.log(`its winnable on row ${i}`);
+				return;
+			}
+		}
+	}
+	for (let i = 0; i < gameState.colPieces.length; i++) {
+		if (
+			gameState.colPieces[i].length < 6 &&
+			gameState.colPieces[i][gameState.colPieces[i].length - 1] ===
+				'red' &&
+			gameState.colPieces[i][gameState.colPieces[i].length - 2] ===
+				'red' &&
+			gameState.colPieces[i][gameState.colPieces[i].length - 3] === 'red'
+		) {
+			computer.target.cellIndex = i;
+			dropPiece(computer, true);
+			console.log(`its loseable on col ${i}`);
+			return;
+		}
+	}
+	for (let i = 0; i < gameState.rowPieces.length; i++) {
+		for (let k = 0; k < 4; k++) {
+			let isTherePieceUnder;
+			if (i === 0) {
+				isTherePieceUnder = true;
+			} else {
+				isTherePieceUnder =
+					gameState.rowPieces[i - 1][k + 3] !== undefined;
+			}
+			if (
+				gameState.rowPieces[i][k] === 'red' &&
+				gameState.rowPieces[i][k + 1] === 'red' &&
+				gameState.rowPieces[i][k + 2] === 'red' &&
+				gameState.rowPieces[i][k + 3] === undefined &&
+				isTherePieceUnder
+			) {
+				computer.target.cellIndex = k + 3;
+				dropPiece(computer, true);
+				console.log(`its loseable on row ${i}`);
+				return;
+			}
+		}
+	}
+	dropPiece(computer, true);
 }
 
 //Function Calls
-makeBoard();
 //Debugging
-let array = [1, 2, 3, 4, 5, 6];
